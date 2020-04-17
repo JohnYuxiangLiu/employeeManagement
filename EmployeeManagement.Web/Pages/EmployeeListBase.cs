3241ddae764e50beb368316948efacfd68ce2e32
unknown <you@example.com>
2020-04-17 17:18:21 +1200

    init

---
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+    }
+}
