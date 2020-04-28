4eddbee28ba2e19b91e7ef46e611ca227aadff26
john <you@example.com>
2020-04-28 14:02:02 +1200

    databindingDemo razor

---
+++ EmployeeManagement.Web/Pages/DatabindingDemoBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DatabindingDemoBase:ComponentBase
+    {
+        protected string Name { get; set; } = "Tom";
+        protected string Gender { get; set; } = "Male";
+        protected string Color { get; set; }="background-color:red";
+
+        //two way data binding method
+        protected void dataBind(ChangeEventArgs e)
+        {
+            Name = e.Value.ToString();
+        }
+    }
+}
